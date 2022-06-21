4b100e3b505f86841b360c6e5bd9d1d5ce8468b7
Hannah Chea <cheahannah@berkeley.edu>
2022-06-21 08:53:46 -0700

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-3e674ae3d53086fa94c2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[306],{1367:function(t,e,n){var r=n(5318);e.__esModule=!0,e.default=void 0;var a=r(n(5354)),u=r(n(7294)),l=function(t){function e(){return t.apply(this,arguments)||this}return(0,a.default)(e,t),e.prototype.render=function(){return u.default.createElement(u.default.Fragment,null)},e}(u.default.Component),o=l;e.default=o}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-3e674ae3d53086fa94c2.js.map
---
+++ component---src-pages-privacy-js-024a3563d79392da026f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[844],{8897:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),a=n(866);function l(){return r.createElement(r.Fragment,null,r.createElement(a.pQ,{title:"Privacy Policy",useTitleTemplate:!0,noIndex:!0}),r.createElement(a.T3,null,r.createElement(a.Ek,{sectionId:"privacy",heading:"Privacy Policy"})))}}}]);
+//# sourceMappingURL=component---src-pages-privacy-js-024a3563d79392da026f.js.map
